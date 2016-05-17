ba0e96b9a9b75ea9ff75b019eda8e0a04a3f720a
Alexey Kasyanchuk <degitx@gmail.com>
2016-05-17 15:34:09 +0300

    использование min версий

---
+++ gulpfile.js
@@ -0,0 +1,22 @@
+var gulp = require('gulp');
+
+gulp.task('beatify-js', function() {
+    var prettify = require('gulp-jsbeautifier');
+
+    return gulp.src(['./*.js'])
+        .pipe(prettify({
+            indentWithTabs: true,
+            braceStyle: "expand"
+        }))
+        .pipe(gulp.dest('./'));
+});
+
+gulp.task('compress', function() {
+    var uglify = require('gulp-uglify');
+
+    return gulp.src('./main.js')
+        .pipe(uglify())
+        .pipe(gulp.dest('./public/js'));
+});
+
+gulp.task('default', ['beatify-js', 'compress']);
